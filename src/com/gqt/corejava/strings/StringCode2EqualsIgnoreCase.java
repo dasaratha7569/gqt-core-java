ca429d5d39a24f3417d88f8d4502d0619a7dbfc7
dasaratha7569 <nsarathreddyavm@gmail.com>
2025-08-01 09:07:22 +0530

    load all string programs

---
+++ src/com/gqt/corejava/strings/StringCode2EqualsIgnoreCase.java
@@ -0,0 +1,52 @@
+package com.gqt.corejava.strings;
+
+public class StringCode2EqualsIgnoreCase {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str1="RAMA";
+		String str2="RAMA";
+		String str3="rama";
+		String str4="SITA";
+		System.out.println("Case-1 : Same strings..");
+		if(str1.equalsIgnoreCase(str2)==true) {
+			System.out.println("Strings equal..");
+		} else {
+			System.out.println("Strings not equal");
+		}
+		System.out.println("-------------");
+		if(str1==str2) {
+			System.out.println("Refernces equal..");
+		} else {
+			System.out.println("Refernces not equal");
+		}
+		System.out.println("==============");
+		System.out.println("Case-2 : Different strings..");
+		if(str1.equalsIgnoreCase(str4)==true) {
+			System.out.println("Strings equal..");
+		} else {
+			System.out.println("Strings not equal");
+		}
+		System.out.println("-------------");
+		if(str1==str4) {
+			System.out.println("Refernces equal..");
+		} else {
+			System.out.println("Refernces not equal");
+		}
+		System.out.println("==============");
+		System.out.println("Case-3 : Same strings but different case..");
+		if(str1.equalsIgnoreCase(str3)==true) {
+			System.out.println("Strings equal..");
+		} else {
+			System.out.println("Strings not equal");
+		}
+		System.out.println("-------------");
+		if(str1==str3) {
+			System.out.println("Refernces equal..");
+		} else {
+			System.out.println("Refernces not equal");
+		}
+		System.out.println("==============");
+	}
+
+}
