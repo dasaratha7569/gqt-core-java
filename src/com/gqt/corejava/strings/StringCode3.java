d7a62582e542755a188b0dc07172776bc5832364
dasaratha7569 <nsarathreddyavm@gmail.com>
2025-08-04 07:05:24 +0530

    load all the string programs

---
+++ src/com/gqt/corejava/strings/StringCode3.java
@@ -0,0 +1,52 @@
+package com.gqt.corejava.strings;
+//object based representation
+public class StringCode3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str1=new String("RAMA");
+		String str2=new String("RAMA");
+		String str3=new String("rama");
+		String str4=new String("SITA");
+		System.out.println("Case-1 : Same strings..");
+		if(str1.equals(str2)==true) {
+			System.out.println("Strings equal..");
+		} else {
+			System.out.println("Strings not equal");
+		}
+		System.out.println("-------------");
+		if(str1==str2) {
+			System.out.println("Refernces equal..");
+		} else {
+			System.out.println("Refernces not equal");
+		}
+		System.out.println("==============");
+		System.out.println("Case-2 : Different strings..");
+		if(str1.equals(str4)==true) {
+			System.out.println("Strings equal..");
+		} else {
+			System.out.println("Strings not equal");
+		}
+		System.out.println("-------------");
+		if(str1==str4) {
+			System.out.println("Refernces equal..");
+		} else {
+			System.out.println("Refernces not equal");
+		}
+		System.out.println("==============");
+		System.out.println("Case-3 : Same strings but different case..");
+		if(str1.equals(str3)==true) {
+			System.out.println("Strings equal..");
+		} else {
+			System.out.println("Strings not equal");
+		}
+		System.out.println("-------------");
+		if(str1==str3) {
+			System.out.println("Refernces equal..");
+		} else {
+			System.out.println("Refernces not equal");
+		}
+		System.out.println("==============");
+	}
+
+}
